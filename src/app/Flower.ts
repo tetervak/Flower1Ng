6d68cbb1a1de49457625c0e01cb1b1d025991f66
Alex Tetervak <tetervak@sheridancollege.ca>
2019-10-31 17:02:20 -0400

    initial commit

---
+++ src/app/Flower.ts
@@ -0,0 +1,13 @@
+export class Flower {
+  public label: string;
+  public imgSrc: string;
+  constructor(name) {
+    this.label = Flower.capitalizeFirstLetter(name);
+    this.imgSrc = `../assets/images/flowers/${name}_large.jpeg`;
+  }
+
+  // make the first letter capital
+  private static capitalizeFirstLetter( /* string */ str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Flower} from './Flower';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Flower1Ng';
+  // to initialize the flowers
+  private static names: string[] = ['aster', 'carnation', 'daffodil', 'orchid', 'rose'];
+  // all flower objects
+  public flowers: Flower[];
+  // the selected flower
+  public flower: Flower;
+  // noinspection JSMismatchedCollectionQueryUpdate
+  private images: HTMLImageElement[];
+  public title = 'Flower 1 Ng';
+  constructor() {
+    this.flowers = [];
+    this.images = [];
+    this.makeFlowers();
+    this.preloadImages();
+    this.selectFlower(0);
+  }
+  // make flower objects
+  private makeFlowers() {
+    AppComponent.names.forEach(name => {
+      this.flowers.push(new Flower(name));
+    });
+  }
+  // preload the images from the server
+  private preloadImages() {
+    this.flowers.forEach(flower => {
+      const image: HTMLImageElement = new Image();
+      image.src = flower.imgSrc;
+      this.images.push(image);
+    });
+  }
+  // show the flower by the index
+  public selectFlower(index) {
+    this.flower = this.flowers[index];
+  }
 }
